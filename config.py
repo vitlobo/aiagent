ae8d5ecf1d5ba6a24c78fad1fa021512e9c73432
vitlobo <awhite1196@proton.me>
2025-08-11 23:30:53 -0400

    CH4: Added ability for AI to call functions to read files and directories, write to files within limited directory scope, and run .py files within limited directory scope.

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
+MODEL = "gemini-2.0-flash-001"
+WORKING_DIR = "./calculator"
 MAX_CHARS = 10_000
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from call_function import available_functions, call_function
+from config import MODEL
+from prompts import system_prompt
+
 
 def main():
     load_dotenv()
@@ -18,7 +22,7 @@ def main():
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -38,13 +42,33 @@ def main():
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages
+        model=MODEL,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 
 if __name__ == "__main__":
